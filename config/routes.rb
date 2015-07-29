ab0d9203a1699c6630c62cbf04604ea7fe2f78e5
Dan <dangarbage15@yahoo.com>
2015-07-29 16:24:51 +0000

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,40 @@
 Rails.application.routes.draw do
+  root "users#index"
+  
+  # Routes for the User resource:
+  # CREATE
+  get '/new_user' => 'users#new'
+  get '/create_user' => 'users#create'
+
+  # READ
+  get '/users' => 'users#index'
+  get '/users/:id' => 'users#show'
+
+  # UPDATE
+  get '/users/:id/edit' => 'users#edit'
+  get '/users/:id/update' => 'users#update'
+
+  # DELETE
+  get '/users/:id/destroy' => 'users#destroy'
+  #------------------------------
+
+  # Routes for the Bookmark resource:
+  # CREATE
+  get '/new_bookmark/:id' => 'bookmarks#new'
+  get '/create_bookmark' => 'bookmarks#create'
+
+  # READ
+  get '/bookmarks' => 'bookmarks#index'
+  get '/bookmarks/:id' => 'bookmarks#show'
+
+  # UPDATE
+  get '/bookmarks/:id/edit' => 'bookmarks#edit'
+  get '/bookmarks/:id/update' => 'bookmarks#update'
+
+  # DELETE
+  get '/bookmarks/:id/destroy' => 'bookmarks#destroy'
+  #------------------------------
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
